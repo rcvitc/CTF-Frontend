46adcaad6025b3ee3f854bc3e1c37e1953cf6486
dipsitarout <dipsitarout@gmail.com>
2025-08-14 21:31:37 +0530

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QuestionPage } from './components/question';
+import Leaderboard from './components/Leaderboard'; // adjust path if needed
 
 function App() {
   return (
-    <div className="App">
-      <QuestionPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Home route */}
+          <Route path="/" element={<QuestionPage />} />
+
+          {/* Leaderboard route */}
+          <Route path="/leaderboard" element={<Leaderboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
